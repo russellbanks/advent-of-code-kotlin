892af8baa6aee63c98bc2b313d0d0e91b0fb591e
Russell Banks <Russell@bandev.uk>
2021-12-12 00:06:04 +0000

    Cleanup Day 1

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -3,22 +3,12 @@ package day1
 import java.io.File
 
 fun main() {
-    fun part1(input: List<Int>): Int {
-        return input.windowed(2).count { (a, b) -> a < b }
-    }
+    fun part1(input: List<Int>): Int = input.windowed(2).count { (a, b) -> a < b }
 
-    // A + B + C <=> B + C + D
-    fun part2(input: List<Int>): Int {
-        return input
-            .windowed(4)
-            .count {
-                it[0] < it[3]
-            }
-    }
+    fun part2(input: List<Int>): Int = input.windowed(4).count { it[0] < it[3] }
 
-    val input = File("src/day1/input.txt")
-        .readLines()
-        .map { it.toInt() }
-    println("Part 1: ${part1(input)}")
-    println("Part 2: ${part2(input)}")
+    File("src/day1/input.txt").readLines().map { it.toInt() }.also {
+        println("Part 1: ${part1(it)}")
+        println("Part 2: ${part2(it)}")
+    }
 }
